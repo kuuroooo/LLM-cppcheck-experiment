567f928d3776337a8f886d90daf8afb8cc3f51f2
kuuroooo <90619410+kuuroooo@users.noreply.github.com>
2025-01-15 18:11:30 +0100

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,14 +1,14 @@
 import json
 import os
 
-input_file = "primevul_train.jsonl"  # Replace with your file path
+input_file = "primevul_train.jsonl" 
 output_dir = "extracted_c_snippets"
 
 os.makedirs(output_dir, exist_ok=True)
 
 with open(input_file, "r") as infile:
     for i, line in enumerate(infile):
-        if i < -1 or i > 20:  # Skip the first 10 snippets and stop after the 20th snippet
+        if i < -1 or i > 20:  
             continue
         record = json.loads(line)
         func_code = record.get("func")
